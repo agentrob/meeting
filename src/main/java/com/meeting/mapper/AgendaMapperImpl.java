977fb48c26519c2c382a0f5e8632d99cdd149ad3
unknown <624378314@qq.com>
2017-10-13 11:22:58 +0800

    test by srs

---
+++ src/main/java/com/meeting/mapper/AgendaMapperImpl.java
@@ -0,0 +1,31 @@
+package com.meeting.mapper;
+
+import com.meeting.pojo.Agenda;
+import com.meeting.utils.JSonUtil;
+
+/**
+ * User: FlowingFire
+ * Date: 2017/9/30 0030
+ * Time: 14:59
+ * Description:
+ *
+ *
+ */
+public class AgendaMapperImpl implements AgendaMapper {
+    @Override
+    public boolean add(Agenda agenda) {
+       boolean flag= JSonUtil.saveAgenda(agenda);
+       return flag;
+    }
+
+    @Override
+    public Agenda findAgendaByTittle(String title) {
+        Agenda agenda=JSonUtil.loadAgenda(title);
+        return agenda;
+    }
+
+    @Override
+    public void delete(String title) {
+        JSonUtil.deleteAgenda(title);
+    }
+}
